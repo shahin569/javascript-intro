f17386dca95eb5cfd48593b776f550f182bef33a
shahin569 <sahin.zaarasara@gmail.com>
2023-08-08 02:29:35 +0600

    array starts

--- introduction.js
+++ introduction.js
@@ -27,4 +27,28 @@ var convertMarksFloat = parseFloat(marksFloat); // to convert a sting float numb
 
 // console.log(convertMarksFloat); // output is a float number
 
+var customTypesCheck = 22;
+var customTypesCheckIntgr ='test';
+var customTypesCheckBoolean = true;
+
+// console.log(typeof customTypesCheck); // to check the type of a vaule of a varriable, we need to use typeof 
+
+// console.log(typeof customTypesCheckIntgr); // check Intesger type
+
+// console.log(typeof customTypesCheckBoolean); // Check Boolean
+
+var numb1 = 0.1;
+var numb2 = 0.2;
+
+var sum = numb1 + numb2;
+sum = sum.toFixed(1) // to show certain number after the dot, dosomiker pot koto sonkha hobe seta dekhar jonno toFixed(1)
+sum = parseFloat(sum) // to convert float string number to a float
+
+// console.log(sum); 
+
+
+//******************** javascript Array*/
+
+var arrStarts = ['rahim', 'karim', 'habib', 'sabid', 'rabid', 'devid'];
+console.log(arrStarts);
 
