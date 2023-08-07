35b4716163cee031fe7d3ab6bc1fc07d601a1f82
shahin569 <sahin.zaarasara@gmail.com>
2023-08-08 03:10:29 +0600

    pop push shift unshift practice

--- introduction.js
+++ introduction.js
@@ -51,12 +51,35 @@ sum = parseFloat(sum) // to convert float string number to a float
 
 var arrStarts = ['rahim', 'karim', 'habib', 'sabid', 'rabid', 'devid'];
 var numberArr = [14, 32, 62, 76, 95, 12, 55, 97]
-console.log(arrStarts);
+// console.log(arrStarts);
 
-var arrIndexTest = arrStarts[0];
+var arrIndexTest = arrStarts[0]; // array index starts from 0 'zero'
 var arrLengthTest = arrStarts.length;  // array length janar jonno arrayname.length dite hoi
 // console.log(arrStarts[0]); // to find an array vaule using index value// index value diye array er value ber korr niom arrayName[0];
 // console.log(arrLengthTest);
 // console.log(numberArr[5]);
+arrStarts[0] = 'hablu'; // index diye kono value ke replace korar jonno aivabe likhete hoi, index er vetore value add korar jonno aitar surute variable dewa lagena.
+// console.log(arrStarts);
+// console.log(arrStarts.indexOf('habib')); // indexOf bebohar kora hoi kono akta array value er index numbertake ber korar jonno
+var arrIndexofFunction = arrStarts.indexOf('habib');
+// console.log(arrIndexofFunction);
+
+
+
+
+//******************** javascript add or remove elemento from array using push and pop, Shift, Unshift*/
+
+var pushPopvar = ['rahim', 'karim', 'habib', 'sabid', 'rabid', 'devid'];
+// pushPopvar.push('newnamebypush1'); // push is to add a new array value at the end
+// console.log(pushPopvar);
+pushPopvar.pop(); // pop diye array er moddhe theke last element ke ber kore dewa hoi.
+// console.log(pushPopvar);
+
+// pushPopvar.shift(); // shift diye array er fist value take remove kore dewa hoi
+// pushPopvar.unshift('hellounshift'); // unshift diye array er fist a akta new value dewa hoi
+
+console.log(pushPopvar);
+
+
 
 
