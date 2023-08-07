33028886fbb2aa7a658f2ea35a2d2d95bebc9f74
shahin569 <sahin.zaarasara@gmail.com>
2023-08-08 00:13:19 +0600

    parsInit and parseFloat Tested

--- introduction.js
+++ introduction.js
@@ -1,4 +1,4 @@
-//variable declaration
+//**variable declaration
 
 var price = 11;
 // price += 9; //short hand increase
@@ -6,4 +6,25 @@ var price = 11;
 
 price--; //to decrease 1 by 1
 
-console.log(price);
+// console.log(price); // 10 is the final result.
+
+
+// ***Make numbers String to a number
+
+var marks = '31';
+
+// console.log(marks); // output is a string
+
+var convertToNumber = parseInt(marks);
+
+// console.log(convertToNumber); // output is a number which is converted from a string number. 
+
+var marksFloat = '31.33';
+
+// console.log(marksFloat); // output is a float string number
+
+var convertMarksFloat = parseFloat(marksFloat); // to convert a sting float number we need to use parseFloat
+
+// console.log(convertMarksFloat); // output is a float number
+
+
