094d5fb038cb3f81e2738fc4cd5889318bf7d126
shahin569 <sahin.zaarasara@gmail.com>
2023-08-09 02:01:52 +0600

    loop reverse practice

---
+++ loop-reverse.js
@@ -0,0 +1,13 @@
+// kono loop ke reverse way te chalanor jonno jevabe loop use kora lage setar example
+
+var num = 10;
+
+while (num >= 1) {
+    console.log(num);
+    num--;
+}
+
+for (let index = 10; index >= 1; index--) {
+    console.log(index);
+    
+}
