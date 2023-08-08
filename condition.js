40a01be524fee4268d9a584251abfa5063e0ecf7
shahin569 <sahin.zaarasara@gmail.com>
2023-08-08 13:24:30 +0600

    grading system condition tested

--- condition.js
+++ condition.js
@@ -1,9 +1,48 @@
-var iphonePrice = 90000;
-var myBudget = 50000;
+// var iphonePrice = 90000;
+// var myBudget = 50000;
 
 
-if (iphonePrice < myBudget) {
-    console.log('i am going to buy iphone');
-} else {
-    console.log('i will buy iphone next year');
+// if (iphonePrice < myBudget) {
+//     console.log('i am going to buy iphone');
+// } else {
+//     console.log('i will buy iphone next year');
+// }
+
+
+// var isGraduated = true;
+// var salary = 70000;
+// var car = 1;
+
+// if (isGraduated == true && salary >= 50000 && car == 0 ) {
+//     console.log('lets get married');    
+// } else if(isGraduated == true && salary >= 50000 && car == 0 ){
+//     console.log('lets try get married');
+    
+// } else if(isGraduated == true || salary > 50000 && car == 1){
+//     console.log('cholo akhoni biye kore feli');
+    
+// } else {
+//     console.log ('You are not qualified');
+// }
+
+var myMarks = 85;
+var tomMarks = 66;
+var janeMarks = 95;
+var peterMarks = 56;
+var johnMarks = 40;
+var marks = 95;
+
+if(marks >= 80 && marks ){
+    console.log ('A grade');
+} else if(marks >=60 && marks <= 79){
+    console.log('B grade');
+} else if(marks >= 50 && marks <= 69){
+    console.log('C grade');
+} else if(marks >= 40 && marks <= 49){
+    console.log('D grade');
+} else if(marks <= 39 && marks >= 0){
+    console.log('tumi fail korcho');
+}
+else{
+    console.log('Please input a number between 0 to 100 ');
 }
