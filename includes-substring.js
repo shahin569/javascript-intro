5f9681c26424d9364031c98c8eaebe97eb18638b
shahin569 <sahin.zaarasara@gmail.com>
2023-08-09 01:15:27 +0600

    include and substring practice

---
+++ includes-substring.js
@@ -0,0 +1,12 @@
+// include is use to find a portion of a string if it is there or not. 
+
+let includeCheckArr= "this is my first test using include to find some portion of an array";
+
+let result = includeCheckArr.includes('find');
+
+// console.log(result);
+
+
+let arrSubstring = "this is a test of substring";
+let resultSubstring = arrSubstring.substring(3, 6);
+console.log(resultSubstring);
