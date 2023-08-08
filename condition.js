c93f125a8e40dd37f54f6602c21cf72a5b36f573
shahin569 <sahin.zaarasara@gmail.com>
2023-08-08 12:48:28 +0600

    if else statment test

---
+++ condition.js
@@ -0,0 +1,9 @@
+var iphonePrice = 90000;
+var myBudget = 50000;
+
+
+if (iphonePrice < myBudget) {
+    console.log('i am going to buy iphone');
+} else {
+    console.log('i will buy iphone next year');
+}
