a2806032937a986d3c9d022b53ae35f93ae66470
shahin569 <sahin.zaarasara@gmail.com>
2023-08-09 13:33:04 +0600

    function practic

---
+++ functions.js
@@ -0,0 +1,22 @@
+// function exercise
+
+function sumTest(a, b, c, d) {
+    var sum = (a+b+c+d); 
+    console.log(sum);
+
+}
+
+// sumTest(1, 3, 4, 5);
+
+function add(numb1, numb2) {
+    // console.log(numb1, numb2);    
+    var sum = numb1 + numb2;
+    return sum;
+}
+
+var firstNumber = 20;
+var seconNumber = 80
+var total  = add(firstNumber, seconNumber);
+
+console.log(total);
+
