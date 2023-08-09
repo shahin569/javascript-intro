3a11595027d3090de2266c37add54aa0025aac4a
shahin569 <sahin.zaarasara@gmail.com>
2023-08-10 01:21:21 +0600

    eidt

---
+++ factorial.js
@@ -0,0 +1,12 @@
+
+// var sumNumbers = 1;
+// for (let index = 1; index <= 10; index++) {
+//     var numberGet = index;
+
+//     console.log(numberGet);
+//     }
+let i =0;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
