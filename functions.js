02b161d6d73b1fd7bb9b1fefba27dde6061fc52b
shahin569 <sahin.zaarasara@gmail.com>
2023-08-09 14:00:28 +0600

    function created

--- functions.js
+++ functions.js
@@ -17,6 +17,30 @@ function add(numb1, numb2) {
 var firstNumber = 20;
 var seconNumber = 80
 var total  = add(firstNumber, seconNumber);
+// console.log(total);
 
-console.log(total);
+
+// per singara price 10, how many singara i can buy at 40 taka
+
+function singara(singaraTaka) {
+    var singaraPrice = 10;
+    var sum = singaraTaka / singaraPrice;
+    return sum;  
+}
+
+var taka = 40;
+var singaraTakaPaid = singara(taka);
+console.log(singaraTakaPaid);
+
+
+function eggPamu(eggTaka) {
+    var pricePerEgg = 10;
+    var eggCalculation = eggTaka / pricePerEgg;
+    return eggCalculation;
+    
+}
+
+var eggDam = 50;
+var eggGetFromShop = eggPamu(eggDam);
+console.log(eggGetFromShop);
 
